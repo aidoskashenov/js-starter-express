d8e29ff2e2658e0e3b31719742e5f16c5202c104
Aidos <aidos_ka@yahoo.com>
2020-08-18 14:48:29 -0500

    Add Style Form scrpts to create POST data request

---
+++ client/scripts.js
@@ -0,0 +1,27 @@
+const postDeets = async(deets) => {
+  const res = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      'Content-Type':'application/json'
+
+    }
+    body: JSON.stringify(deets)
+  })
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  event.preventDefault();
+
+  // const test = Array.from(event.target.elements).map(el => {
+  //   console.log(el.dataset.form)
+  // })
+
+  postDeets(
+    Array.from(event.target.elements)
+      .filter(({ dataset }) => dataset.form)
+      .reduce((submissionDeets, currentEl) => {
+        submissionDeets[currentEl.id] = currentEl.value;
+        return submissionDeets;
+      }, {})
+  );
+});
